8a54cc03e444f3c7459437e7d44b0558b54606eb
thomasbricchi <dev@wideside.ch>
2020-07-06 17:00:39 +0200

    end challenge 3

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -1,3 +1,27 @@
 import { createReducer, on, Action, createSelector } from "@ngrx/store";
 import { BookModel, calculateBooksGrossEarnings } from "src/app/shared/models";
 import { BooksPageActions, BooksApiActions } from "src/app/books/actions";
+
+
+export interface State {
+  collection: BookModel[];
+  activeBookId: string | null;
+}
+
+export const initialState: State = {
+  collection: [],
+  activeBookId: null
+}
+
+
+export const booksReducer = createReducer(
+  initialState,
+  on(BooksPageActions.enter, BooksPageActions.clearSelectedBook,
+    (state, action) => {
+      return {...state, activeBooksId: null}
+    }),
+  on(BooksPageActions.selectBook,
+    (state, action) => {
+      return {...state, activeBooksId: action.bookId}
+    }),
+)
